fc741315b9c847cb87a85db49237033d8fdfb009
Edwin Hollands <e.c.hollands@hotmail.co.uk>
2026-03-03 14:40:10 +0000

    setting up data split

--- EmailTransformerToy.py
+++ EmailTransformerToy.py
@@ -1,3 +1,5 @@
+from random import randint
+from mpmath import rand
 import torch
 #First we download our dataset
 
@@ -5,7 +7,8 @@ import torch
 #with open('emails.txt', 'r') as file:
     #data = file.read()
 
-text = "hello world"
+#for now, let's keep it simple
+text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam hendrerit nisi sed sollicitudin pellentesque. Nunc posuere purus rhoncus pulvinar aliquam. Ut aliquet tristique nisl vitae volutpat. Nulla aliquet porttitor venenatis. Donec a dui et dui fringilla consectetur id nec massa. Aliquam erat volutpat. Sed ut dui ut lacus dictum fermentum vel tincidunt neque. Sed sed lacinia lectus. Duis sit amet sodales felis. Duis nunc eros, mattis at dui ac, convallis semper risus. In adipiscing ultrices tellus, in suscipit massa vehicula eu."
 
 #let's check a sample
 print(f"First hundred characters are: {text[:100]}")
@@ -21,8 +24,27 @@ encode = lambda s: [vocab.index(c) for c in s]
 decoded_chars = lambda n: [vocab[k] for k in n]
 decode = lambda n: "".join(decoded_chars(n))
 
-print(encode("hello world"))
-print(decode(encode("hello world")))
-
 #lets encode our data as a tensor
-data = torch.tensor(encode(text), dtype = torch.long)
+data = torch.tensor(encode(text), dtype = torch.int64)
+#and split it into training and validation sets
+n=int(0.9*len(data))
+train_data = data[:n]
+val_data = data[n:]
+
+#now lets pick a block size i.e. 'context length'
+block_size = 10
+
+#we will run multiple samples in parallel
+batch_size = 4
+
+#now make a process to pull out a chunk of the data
+def sample(set):
+    data = train_data if set == 'train' else val_data
+    samples=torch.randint(len(data)-block_size, (batch_size,))
+    inputs = torch.stack([data[i:i+block_size] for i in samples])
+    targets = torch.stack([data[i+1:i+1+block_size] for i in samples])
+    return inputs, targets
+
+
+
+
