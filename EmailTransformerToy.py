104f48eec52f7c535cf5969dd4dc5a1736997059
Edwin Hollands <e.c.hollands@hotmail.co.uk>
2026-03-16 19:23:39 +0000

    loss tracking

--- EmailTransformerToy.py
+++ EmailTransformerToy.py
@@ -1,27 +1,33 @@
 import torch
 import torch.nn as nn
-#First we download our dataset
 
-#now retrieve it to process
+device        = 'cuda' if torch.cuda.is_available() else 'cpu' # use gpu if poss
+#PARAMETERS
+block_size = 32   #i.e. 'context length', also called time steps?
+batch_size = 32   #we will run multiple samples in parallel
+learn_rate = 0.001
+iters = 4000
+interval = 300
+prompt = 'hello world'
+gen_length = 50
+
+#First we get our dataset
 with open('input.txt', 'r') as file:
     text = file.read()
-
-#for now, let's keep it simple
-#text = "Hello world. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam hendrerit nisi sed sollicitudin pellentesque. Nunc posuere purus rhoncus pulvinar aliquam. Ut aliquet tristique nisl vitae volutpat. Nulla aliquet porttitor venenatis. Donec a dui et dui fringilla consectetur id nec massa. Aliquam erat volutpat. Sed ut dui ut lacus dictum fermentum vel tincidunt neque. Sed sed lacinia lectus. Duis sit amet sodales felis. Duis nunc eros, mattis at dui ac, convallis semper risus. In adipiscing ultrices tellus, in suscipit massa vehicula eu."
-
-#let's check a sample
-#print(f"First hundred characters are: {text[:100]}")
 
 #Find our vocabulary in terms of characters
 vocab = sorted(list(set(text)))
 vocab_size = len(vocab)
 
-#print(f"Vocabulary size: {vocab_size}, list of characters: {vocab}")
-
 #now we need to build an encoder and decoder
-encode = lambda s: [vocab.index(c) for c in s]
-decoded_chars = lambda n: [vocab[k] for k in n]
-decode = lambda n: "".join(decoded_chars(n))
+
+#encode = lambda s: [vocab.index(c) for c in s] <- O(n), not good
+
+#O(1) version using dictionaries
+stoi = {ch: i for i, ch in enumerate(vocab)}
+itos = {i: ch for i, ch in enumerate(vocab)}
+encode = lambda s: [stoi[c] for c in s]
+decode = lambda l: ''.join([itos[i] for i in l])
 
 #lets encode our data as a tensor
 data = torch.tensor(encode(text), dtype = torch.int64)
@@ -30,26 +36,21 @@ n=int(0.9*len(data))
 train_data = data[:n]
 val_data = data[n:]
 
-#now lets pick a block size i.e. 'context length', also called time steps?
-block_size = 8
-
-#we will run multiple samples in parallel
-batch_size = 2
-
 #now make a process to pull out a chunk of the data. Input string 'train' for training or 'val' for validation.
-def sample(set):
-    data = train_data if set == 'train' else val_data
+def sample(dataset):
+    data = train_data if dataset == 'train' else val_data
     samples=torch.randint(len(data)-block_size, (batch_size,))
     inputs = torch.stack([data[i:i+block_size] for i in samples])
     targets = torch.stack([data[i+1:i+1+block_size] for i in samples])
+    inputs = inputs.to(device)
+    targets = targets.to(device)
     # we create 2d tensors [batches*[samples]]
     return inputs, targets
 
 #so we feed samples into the transformer
-
 class BigramLanguageModel(nn.Module):
     def __init__(self, vocab_size):
-        super().__init__() # number of embeddings = embedding dim = vocab size so each token is embedded as a vocab-size dimensional vector
+        super().__init__() # 'square' since number of embeddings = embedding dim = vocab size so each token is embedded as a vocab-size dimensional vector
         self.token_embedding_table = nn.Embedding(vocab_size, vocab_size)
 
     def forward(self, idx, targets=None):
@@ -57,13 +58,13 @@ class BigramLanguageModel(nn.Module):
         logits = self.token_embedding_table(idx)  # batch*time*vocab_size
 
         if targets is None:
-            return logits, None
+            return logits, None #for generation
 
         B, T, C = logits.shape # B is batch size, T is sequence length or time, C is vocab size
         logits = logits.view(B*T, C) # B*T = number of tokens, vocab size. Reshaping so cross entropy sees vocab as 2nd dim
         targets = targets.view(B*T)
         loss = nn.functional.cross_entropy(logits, targets)
-        return logits, loss
+        return logits, loss #for training
 
         #we need a way to generate
     def generate(self, idx, max):
@@ -71,27 +72,28 @@ class BigramLanguageModel(nn.Module):
             logits, loss = self(idx)
             logits = logits[:,-1,:] # just take the final prediction since we aren't training
             probabilities = nn.functional.softmax(logits, dim=-1) #softmax to make a distribution
-            next = torch.multinomial(probabilities, num_samples=1) # sample the distribution
-            idx = torch.cat((idx, next), dim=1) # append the new token
+            next1 = torch.multinomial(probabilities, num_samples=1) # sample the distribution
+            idx = torch.cat((idx, next1), dim=1) # append the new token
         return idx
 
-model = BigramLanguageModel(vocab_size)
+model = BigramLanguageModel(vocab_size).to(device)
 
 #Now we need to train
-optimiser = torch.optim.AdamW(model.parameters(), lr=0.001)
+optimiser = torch.optim.AdamW(model.parameters(), lr=learn_rate)
 
-batch_size = 4
-for _ in range(1000):
+for i in range(iters):
+
     optimiser.zero_grad(set_to_none=True)
     inputs, targets = sample('train')
     logits, loss = model(inputs, targets)
     loss.backward()
+    if i % interval == 0:
+        print(f"step {i} the loss is {loss.item()}")
     optimiser.step()
-print(loss.item())
 
 
-inputs = torch.tensor([encode('hello world')], dtype = torch.int64)
-new = decode(model.generate(inputs, 50)[0].tolist())
+inputs = torch.tensor([encode(prompt)], dtype = torch.int64, device=device)
+new = decode(model.generate(inputs, gen_length)[0].tolist())
 print(new)
 
 
